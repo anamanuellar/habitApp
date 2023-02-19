c7d2e2929a74156cf9e3413c775ed235f03af249
anamanuellar <ana.manuellar@gmail.com>
2023-02-19 10:51:19 -0300

    Criando mais rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { prisma } from './lib/prisma'
 
-export async function appRoutes (app: FastifyInstance){
+export async function appRoutes(app: FastifyInstance) {
 
     app.post('/habits', async (request) => {
         const createHabitBody = z.object({
@@ -13,7 +13,7 @@ export async function appRoutes (app: FastifyInstance){
         })
 
 
-        const { title, weekDays} = createHabitBody.parse(request.body)
+        const { title, weekDays } = createHabitBody.parse(request.body)
 
         const today = dayjs().startOf('day').toDate()
 
@@ -32,6 +32,49 @@ export async function appRoutes (app: FastifyInstance){
         })
     })
 
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+        //todos habitos possíveis
+        //habitos que já foram completados
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true,
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits,
+        }
+
+    })
 }
 
 

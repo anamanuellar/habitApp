526b4e1e1b8661f9f9e51b0045d869be12194f41
anamanuellar <ana.manuellar@gmail.com>
2023-02-19 11:24:23 -0300

    Criando componente Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/global.css';
-import logoImage from './assets/logo.svg'
+import { Header } from './components/Header';
+
 
 //import { Habit } from './components/Habit'
 
@@ -7,16 +8,7 @@ export function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex-col gap-16'>
-      <div className='w-full mx-auto flex items-center justify-between'>
-      <img src={logoImage} alt='Habits' />
-      <button 
-        type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
-        >
-        Novo Hábito
-      </button>
-      </div>
-      
+        <Header />
       </div>
     </div>
   )

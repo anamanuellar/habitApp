c5bb59a08081dc90b50b3df7f35d9124ae467899
anamanuellar <ana.manuellar@gmail.com>
2023-03-18 18:32:23 -0300

    Criando Popover com Radix

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,18 @@
-//interface HabitProps {
-   // completed: number
-//}
+import * as Popover from '@radix-ui/react-popover';
 
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+        <Popover.Portal>
+            <Popover.Content className='min-width-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                Testeeee
+                <Popover.Arrow height={8} width= {16}className='fill-zinc-900'/>
+            </Popover.Content>
+        </Popover.Portal>
+
+
+        </Popover.Root>
+        
     )
 }

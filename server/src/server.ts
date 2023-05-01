256a2700c260acd5959a83d8a677e930cf303e81
anamanuellar <ana.manuellar@gmail.com>
2023-05-01 16:06:27 -0300

    Adicionando o parâmetro host  chamada do listen

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import Fastify from 'fastify'
-import cors from'@fastify/cors'
+import cors from '@fastify/cors'
 import { appRoutes } from './routes'
 
 const app = Fastify()
@@ -7,8 +7,15 @@ const app = Fastify()
 app.register(cors)
 app.register(appRoutes)
 
+//passando um objeto de opções com port e host definidas e assim receber o end completo do servidor como o segundo argumento do callback.
 app.listen({
     port: 3333,
-}).then(() => {
-    console.log('HTTP Server runnning!')
-})
+    host: '0.0.0.0' //aceitar conexões de qualquer endereço IP na rede local
+  }, (err, address) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`Servidor iniciado em ${address}`)
+  })
+  

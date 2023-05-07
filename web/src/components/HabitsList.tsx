56a0452f691d83b99dfa54b0ca2185106d47d190
anamanuellar <ana.manuellar@gmail.com>
2023-05-07 11:03:46 -0300

    Desabilitando em datas passadas

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -2,9 +2,10 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
+import dayjs from 'dayjs';
 
 interface HabitsListProps {
-    date: Date
+    date: Date;
 }
 
 interface HabitsInfo {
@@ -16,41 +17,46 @@ interface HabitsInfo {
     completedHabits: string[]
 }
 
-export function HabitsList({date: Date}:HabitsListProps) {
+export function HabitsList({ date: date }: HabitsListProps) {
     const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
-  
+
     useEffect(() => {
         api.get('day', {
-            params: { 
-                date: Date.toISOString(),
+            params: {
+                date: date.toISOString(),
             }
         }).then(response => {
             setHabitsInfo(response.data)
         })
     })
 
+    const isDateinPast = dayjs(date)
+    .endOf('day')
+    .isBefore(new Date())
+
     return (
-        
+
         <div className='mt-6 flex flex-col gap-3' >
-            {habitsInfo?.possibleHabits.map(habit =>{
-                return(
+            {habitsInfo?.possibleHabits.map(habit => {
+                return (
                     <Checkbox.Root
-                    key={habit.id}
-                    checked={habitsInfo.completedHabits.includes(habit.id)}
-                    className='flex items-center gap-3 group'
-                >
-                   <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                   <Checkbox.Indicator>
-                    <Check size={20}  className='text-white'/>
-                   </Checkbox.Indicator>
-                   </div>
-                    <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                        {habit.title}
-                    </span>
-                </Checkbox.Root>
+                        key={habit.id}
+                        checked={habitsInfo.completedHabits.includes(habit.id)}
+                        disabled={isDateinPast}
+                        className='flex items-center gap-3 group'
+                    >
+                        <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                            <Checkbox.Indicator>
+                                <Check size={20} className='text-white' />
+                            </Checkbox.Indicator>
+                        </div>
+                        <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+                            {habit.title}
+                        </span>
+                    </Checkbox.Root>
                 )
             })}
-           
+
 
         </div>
     )
